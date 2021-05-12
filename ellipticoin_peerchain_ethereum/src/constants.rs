b196427682243475eddcee02fb9329d1ab9217ff
Mason Fischer <mason@kissr.co>
2021-05-12 07:15:22 -0400

    Use Alchemy Transers API for deposits

--- ellipticoin_peerchain_ethereum/src/constants.rs
+++ ellipticoin_peerchain_ethereum/src/constants.rs
@@ -1,41 +1,22 @@
-use ellipticoin_contracts::constants::{BASE_FACTOR, BTC, DAI, ETH, LEVERAGED_BASE_TOKEN};
+use ellipticoin_contracts::constants::{BASE_FACTOR, BTC, C_DAI, DAI, ETH, LEVERAGED_BASE_TOKEN};
 use ellipticoin_types::Address;
 use hex_literal::hex;
 use lazy_static::lazy_static;
-use std::{collections::HashMap, env};
+use std::env;
 
 lazy_static! {
-    pub static ref ELLIPTICOIN_DECIMALS: usize = BASE_FACTOR.to_string().len() - 1;
-    pub static ref DECIMALS: HashMap<Address, usize> = {
-        let mut decimals = HashMap::new();
-        decimals.insert(BTC, 8);
-        decimals.insert(ETH_ADDRESS, 18);
-        // decimals.insert(USD, 18);
-        decimals.insert(LEVERAGED_BASE_TOKEN, 8);
-        decimals.insert(DAI, 18);
-        decimals
-    };
+    pub static ref ELLIPTICOIN_DECIMALS: usize = BASE_FACTOR.to_string().len();
     pub static ref WEB3_URL: String = env::var("WEB3_URL").expect("WEB3_URL not set");
 }
 
 pub static BASE_TOKEN_UNDERLYING_DECIMALS: usize = 28;
 pub static REDEEM_TIMEOUT: u64 = 30;
-pub const TRANSFER_TOPIC: [u8; 32] =
-    hex!("ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef");
 pub const REDEEM_TOPIC: [u8; 32] =
     hex!("ff051e185ca4ab867487cbb2112ad9dcf4b6e45ec93c6c83fe371bfd126d1da6");
-pub const RECEIVED_ETH_TOPIC: [u8; 32] =
-    hex!("4103257eaac983ca79a70d28f90dfc4fa16b619bb0c17ee7cab0d4034c279624");
-
-// pub const BASE_TOKEN_ADDRESS: Address = Address(hex!("5d3a536E4D6DbD6114cc1Ead35777bAB948E3643"));
-pub const BASE_TOKEN_ADDRESS: Address = Address(hex!("6d7f0754ffeb405d23c51ce938289d4835be3b14"));
+pub const LEVERAGED_BASE_TOKEN_ADDRESS: Address = C_DAI;
 pub const EXCHANGE_RATE_CURRENT_SELECTOR: [u8; 4] = hex!("bd6d894d");
 pub const SUPPLY_RATE_PER_BLOCK_SELECTOR: [u8; 4] = hex!("ae9d70b0");
 pub const TOKENS: [Address; 4] = [DAI, BTC, ETH, LEVERAGED_BASE_TOKEN];
-pub const BRIDGE_ADDRESS: Address = Address(hex!("6f246D6B8C0cca9298C685D02dFDA3A666e6e067"));
-// pub const BRIDGE_ADDRESS: Address = Address(hex!("E55faDE7825Ad88581507C51c9f1b33827AaE5E8"));
-// pub const SAFE_ADDRESS: Address = Address(hex!("6bDCC3608e8DF882096A1b4b106C727bd28F141D"));
-// pub const SAFE_ADDRESS: Address = Address(hex!("a478c2975ab1ea89e8196811f51a7b7ade33eb11"));
-pub const SAFE_ADDRESS: Address = Address(hex!("7a250d5630B4cF539739dF2C5dAcb4c659F2488D"));
-// pub const BRIDGE_ADDRESS: Address = Address(hex!("E55faDE7825Ad88581507C51c9f1b33827AaE5E8"));
+pub const BRIDGE_ADDRESS: Address = Address(hex!("E55faDE7825Ad88581507C51c9f1b33827AaE5E8"));
+pub const SAFE_ADDRESS: Address = Address(hex!("5510f178A57C4f4B456d747CdbfcD0A5b1b5473b"));
 pub const ETH_ADDRESS: Address = Address(hex!("0000000000000000000000000000000000000000"));
--- ellipticoin_peerchain_ethereum/src/lib.rs
+++ ellipticoin_peerchain_ethereum/src/lib.rs
@@ -3,11 +3,10 @@ pub mod transaction;
 use ellipticoin_types::Address;
 pub use transaction::*;
 
-use crate::constants::SAFE_ADDRESS;
 use crate::constants::{
-    BASE_TOKEN_ADDRESS, BRIDGE_ADDRESS, DECIMALS, ELLIPTICOIN_DECIMALS, ETH_ADDRESS,
-    EXCHANGE_RATE_CURRENT_SELECTOR, RECEIVED_ETH_TOPIC, REDEEM_TOPIC,
-    SUPPLY_RATE_PER_BLOCK_SELECTOR, TOKENS, TRANSFER_TOPIC, WEB3_URL,
+    BRIDGE_ADDRESS, ELLIPTICOIN_DECIMALS, ETH_ADDRESS, EXCHANGE_RATE_CURRENT_SELECTOR,
+    LEVERAGED_BASE_TOKEN_ADDRESS, REDEEM_TOPIC, SAFE_ADDRESS, SUPPLY_RATE_PER_BLOCK_SELECTOR,
+    TOKENS, WEB3_URL,
 };
 use ellipticoin_contracts::{
     bridge::{Mint, Redeem, Update},
@@ -36,19 +35,25 @@ struct TransfersResult {
 struct Transfer {
     hash: String,
     asset: String,
-    #[serde(deserialize_with = "parse_address2")]
+    #[serde(deserialize_with = "parse_address")]
     from: Option<Address>,
     raw_contract: RawContract,
 }
 
 #[derive(Deserialize, Debug)]
 struct RawContract {
-    #[serde(deserialize_with = "parse_address2")]
+    #[serde(deserialize_with = "parse_address")]
     address: Option<Address>,
     #[serde(deserialize_with = "parse_big_uint")]
     value: BigUint,
     #[serde(deserialize_with = "parse_usize")]
     decimal: usize,
+}
+
+#[derive(Deserialize, Debug)]
+struct RedeemLog {
+    #[serde(deserialize_with = "parse_big_uint")]
+    data: BigUint,
 }
 
 pub async fn poll(latest_block: u64) -> Result<Poll<Update>, surf::Error> {
@@ -57,7 +62,7 @@ pub async fn poll(latest_block: u64) -> Result<Poll<Update>, surf::Error> {
         Ok(Poll::Pending)
     } else {
         let base_token_exchange_rate = eth_call(
-            BASE_TOKEN_ADDRESS,
+            LEVERAGED_BASE_TOKEN_ADDRESS,
             EXCHANGE_RATE_CURRENT_SELECTOR,
             current_block,
         )
@@ -76,71 +81,104 @@ pub async fn poll(latest_block: u64) -> Result<Poll<Update>, surf::Error> {
             block_number: current_block,
             base_token_interest_rate,
             base_token_exchange_rate,
-            mints: vec![],//[
-                // get_mints(SAFE_ADDRESS, from_block, current_block).await?,
-                // get_mints(BRIDGE_ADDRESS, from_block, current_block).await?
-            // ].concat(),
+            mints: [
+                get_mints_to_bridge(from_block, current_block)
+                    .await
+                    .unwrap(),
+                get_mints_to_safe(from_block, current_block).await.unwrap(),
+            ]
+            .concat(),
             redeems: get_redeems(from_block, current_block).await?,
         }))
     }
 }
 
-async fn get_mints(to_address: Address, from_block: u64, to_block: u64) -> Result<Vec<Mint>, surf::Error> {
-    let mut res = surf::post(WEB3_URL.clone())
-        .body(json!(
-         {
-         "id": 1,
-         "jsonrpc": "2.0",
-         "method": "alchemy_getAssetTransfers",
-         "params": [{
-         "fromBlock": format!("0x{}", BigUint::from(from_block).to_str_radix(16)),
-         "toBlock": format!("0x{}", BigUint::from(to_block).to_str_radix(16)),
-         "toAddress": format!("0x{}", hex::encode(to_address)),
-         "contractAddresses": TOKENS.iter().map(|token|
-                 format!("0x{}", hex::encode(&token))
-                 ).collect::<Vec<String>>()
-         }]
-         }
-        ))
-        .await
-        .unwrap();
-
-    let mints = res
-        .body_json::<TransfersResponse>()
-        .await
-        .unwrap()
-        .result
-        .transfers
+async fn get_mints_to_bridge(from_block: u64, to_block: u64) -> Result<Vec<Mint>, surf::Error> {
+    let mints = get_asset_transfers(BRIDGE_ADDRESS, from_block, to_block)
+        .await?
         .iter()
-        .map(|transfer: &Transfer| {
-        Mint (
-            scale_down(transfer.raw_contract.value.clone(), transfer.raw_contract.decimal),
-            transfer.raw_contract.address.unwrap_or(ETH_ADDRESS),
-            transfer.from.unwrap(),
-
-        )
+        .filter_map(|transfer: &Transfer| {
+            if transfer.from.unwrap() == SAFE_ADDRESS {
+                None
+            } else {
+                Some(Mint(
+                    scale_down(
+                        transfer.raw_contract.value.clone(),
+                        transfer.raw_contract.decimal,
+                    ),
+                    transfer.raw_contract.address.unwrap_or(ETH_ADDRESS),
+                    transfer.from.unwrap(),
+                ))
+            }
         })
-        .collect::<Vec<Mint>>();
+        .collect();
     Ok(mints)
 }
 
+async fn get_mints_to_safe(from_block: u64, to_block: u64) -> Result<Vec<Mint>, surf::Error> {
+    let mints = get_asset_transfers(SAFE_ADDRESS, from_block, to_block)
+        .await?
+        .iter()
+        .map(|transfer: &Transfer| {
+            Mint(
+                scale_down(
+                    transfer.raw_contract.value.clone(),
+                    transfer.raw_contract.decimal,
+                ),
+                transfer.raw_contract.address.unwrap_or(ETH_ADDRESS),
+                transfer.from.unwrap(),
+            )
+        })
+        .collect();
+    Ok(mints)
+}
+
+async fn get_asset_transfers(
+    to_address: Address,
+    from_block: u64,
+    to_block: u64,
+) -> Result<Vec<Transfer>, surf::Error> {
+    loop {
+        let mut res = surf::post(WEB3_URL.clone())
+            .body(json!(
+             {
+             "id": 1,
+             "jsonrpc": "2.0",
+             "method": "alchemy_getAssetTransfers",
+             "params": [{
+             "fromBlock": format!("0x{}", BigUint::from(from_block).to_str_radix(16)),
+             "toBlock": format!("0x{}", BigUint::from(to_block).to_str_radix(16)),
+             "toAddress": format!("0x{}", hex::encode(to_address)),
+             "contractAddresses": TOKENS.iter().map(|token|
+                     format!("0x{}", hex::encode(&token))
+                     ).collect::<Vec<String>>()
+             }]
+             }
+            ))
+            .await
+            .unwrap();
+        let transfers_response_string = res.body_string().await?;
+        let transfers_response =
+            match serde_json::from_str::<TransfersResponse>(&transfers_response_string) {
+                Ok(transfers_response) => transfers_response.result.transfers,
+                Err(err) => {
+                    println!("{}: {}", err.to_string(), transfers_response_string);
+                    continue;
+                }
+            };
+        return Ok(transfers_response);
+    }
+}
+
 async fn get_redeems(from_block: u64, to_block: u64) -> Result<Vec<Redeem>, surf::Error> {
-    let logs = get_logs(from_block, to_block, vec![REDEEM_TOPIC]).await?;
+    let logs = get_logs(BRIDGE_ADDRESS, from_block, to_block, vec![REDEEM_TOPIC]).await?;
     Ok(logs
         .iter()
-        .filter_map(|log| {
-            let address =
-                parse_address(&value_to_string(&log.get("address").unwrap()).unwrap()).unwrap();
-            if address == BRIDGE_ADDRESS {
-                let redeem_id =
-                    parse_big_int(&value_to_string(&log.get("data").unwrap().clone()).unwrap())
-                        .unwrap()
-                        .to_u64()
-                        .unwrap();
-                Some(Redeem(redeem_id))
-            } else {
-                None
-            }
+        .cloned()
+        .map(|log| {
+            let redeem_log: RedeemLog = serde_json::value::from_value(log).unwrap();
+            let redeem_id = redeem_log.data.to_u64().unwrap();
+            Redeem(redeem_id)
         })
         .collect())
 }
@@ -153,7 +191,7 @@ fn scale_down(amount: BigUint, decimals: usize) -> u64 {
 
 pub async fn get_base_token_interest_rate(block_number: u64) -> Result<u64, surf::Error> {
     let rate = eth_call(
-        BASE_TOKEN_ADDRESS,
+        LEVERAGED_BASE_TOKEN_ADDRESS,
         SUPPLY_RATE_PER_BLOCK_SELECTOR,
         block_number,
     )
@@ -193,11 +231,17 @@ pub async fn eth_call(
             .await
         {
             Ok(res_hex) => res_hex,
-            Err(_) => continue,
+            Err(err) => {
+                println!("{}", err);
+                continue;
+            }
         };
         let res_hash_map = match res.body_json::<HashMap<String, serde_json::Value>>().await {
             Ok(res_hash_map) => res_hash_map,
-            Err(_) => continue,
+            Err(err) => {
+                println!("{}", err);
+                continue;
+            }
         };
         if res_hash_map.contains_key("result") {
             break serde_json::from_value::<String>(res_hash_map.get("result").unwrap().clone())?;
@@ -245,6 +289,7 @@ pub async fn get_current_block() -> Result<u64, surf::Error> {
 }
 
 async fn get_logs(
+    address: Address,
     from_block: u64,
     to_block: u64,
     topics: Vec<[u8; 32]>,
@@ -257,6 +302,7 @@ async fn get_logs(
                 "jsonrpc": "2.0",
                 "method": "eth_getLogs",
                 "params": [{
+                    "address": format!("0x{}", hex::encode(address)),
                     "fromBlock": format!("0x{}", BigUint::from(from_block).to_str_radix(16)),
                     "toBlock": format!("0x{}", BigUint::from(to_block).to_str_radix(16)),
                     "topics": topics.iter().map(|topic| format!("0x{}", hex::encode(topic))).collect::<Vec<String>>(),
@@ -284,20 +330,7 @@ async fn get_logs(
     }
 }
 
-fn value_to_string(value: &Value) -> Option<String> {
-    serde_json::from_value(value.clone()).ok()
-}
-
-fn parse_address(s: &str) -> Option<Address> {
-    if s == "0x" {
-        return None;
-    }
-
-    let bytes = hex::decode(s.trim_start_matches("0x")).unwrap();
-    Some(Address(bytes[..][bytes.len() - 20..].try_into().unwrap()))
-}
-
-fn parse_address2<'de, D>(deserializer: D) -> Result<Option<Address>, D::Error>
+fn parse_address<'de, D>(deserializer: D) -> Result<Option<Address>, D::Error>
 where
     D: Deserializer<'de>,
 {
@@ -329,19 +362,9 @@ fn parse_usize<'de, D>(deserializer: D) -> Result<usize, D::Error>
 where
     D: Deserializer<'de>,
 {
-    parse_big_uint(deserializer).map(|n| n.to_usize())?.ok_or(serde::de::Error::custom("error parsing bytes"))
-    // let s = String::deserialize(deserializer)?;
-    //
-    // Ok(
-    //     BigUint::parse_bytes(s.trim_start_matches("0x").as_bytes(), 16)
-    //         .ok_or(serde::de::Error::custom("error parsing bytes"))?,
-    // )
-}
-
-fn parse_big_int(s: &str) -> Option<BigUint> {
-    if s == "0x" {
-        return None;
-    }
-
-    Some(BigUint::parse_bytes(s.trim_start_matches("0x").as_bytes(), 16).unwrap())
+    parse_big_uint(deserializer)
+        .map(|n| n.to_usize())?
+        .ok_or(serde::de::Error::custom(
+            "error converting BigUint to usize",
+        ))
 }
